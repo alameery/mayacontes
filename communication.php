82f1ade70f42a221c626df392a804f60cd7a890c
alameery <75560267+alameery@users.noreply.github.com>
2022-10-16 06:34:56 +0300

    Add files via upload

---
+++ communication.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="margin-top:10px;">
+<div class="form-floating mb-1 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1  text-center"><img src="IMAIGES/speedometer.png" alt=""> </div>
+</div>
+</div>
+<body style="margin-top:10px;">
+<div class="form-floating mb-1 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1  text-center"><img src="IMAIGES/logest.png" width="30%" alt=""> </div>
+</div>
+</div>
+<div class="form-floating mb-2 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1 bg-primary text-center"><a href="" style="text-align:center; color:black;font: size 25px;"><img src="IMAIGES/websa.ico" alt=""> الموقع الالكرتوني</a></div>
+</div>
+<div class="form-floating mb-2 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1 bg-primary text-center"><a href="" style="text-align:center; color:black;font: size 25px;"><img src="IMAIGES/whatsapp_32.png" alt=""> التواصل واتساب</a></div>
+</div>
+</div>
+<div class="form-floating mb-2 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1 bg-primary text-center"><a href="" style="text-align:center; color:black;font: size 25px;"><img src="IMAIGES/facebook.ico" alt=""> التواصل فيسبوك</a></div>
+</div>
+</div>
+<div class="form-floating mb-2 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1 bg-primary text-center"><a href="" style="text-align:center; color:black;font: size 25px;"><img src="IMAIGES/snapchat.ico" alt=""> التواصل سناب </a></div>
+</div>
+<div>
+ <div class="form-floating mb-2 container mt-1" style="margin-top:150px ;">
+<div class="p-3 mb-1 bg-primary text-center"><a href="" style="text-align:center; color:black;font: size 25px;"><img src="IMAIGES/twitter.ico" alt=""> التواصل تويتر </a></div>
+</div>
+<div>   
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+</html>
